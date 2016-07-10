e48229d95256ddc5ca5fc16a79ec554fc133eb92
philip30 <philip.arthur30@gmail.com>
2016-07-10 16:47:24 +0900

    Added setup.py

--- chainn/test/__init__.py
+++ chainn/test/__init__.py
@@ -1,3 +1,2 @@
 from chainn.test.tc import TestCase
 from chainn.test.args import Args
-from chainn.test.test_dictionary import TestDictionary
---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+import sys
+
+sys.path.append('./chainn')
+sys.path.append('./test')
+
+install_requires = [
+    'Chainer>=1.10',
+    'numpy>=1.9.0']
+
+setup(
+        name='Chainn',
+        version='1.0.0',
+        description='Neural Network Translation toolkit decoder',
+        long_description='Chainn is a NMT Translation toolkit based on Chainer\
+            It supports a basic encoder-decoder and attentional NMT models.\
+            It also supports language modeling and pos tagging.',
+        author='Philip Arthur',
+        author_email='philip.arthur30@gmail.com',
+        license='MIT License',
+        install_requires=install_requires,
+        packages=[
+            'chainn',
+            'chainn.util',
+            'chainn.model',
+            'chainn.machine',
+            'chainn.util',
+            'chainn.util.output',
+            'chainn.util.io',
+            'chainn.chainer_component',
+            'chainn.chainer_component.functions',
+            'chainn.chainer_component.links',
+            'chainn.classifier',
+            'chainn.model.basic',
+            'chainn.model.nmt',
+            'chainn.model.text',
+            'chainn.test'
+        ],
+        test_suite='chainn_test.suite',
+        platforms='requires Python Chainer Numpy'
+        )
